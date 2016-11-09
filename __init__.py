72f32f1452ec46e359d022bd85676d56528c6a73
Nathanael Farley <nasfarley88@gmail.com>
2016-11-09 21:11:16 +0000

    Restructured, removed unnecessary libraries.

--- __init__.py
+++ __init__.py
@@ -1,7 +1,4 @@
-import tempfile
-import subprocess as sp
 import logging
-import re
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import MessageHandler, Filters, CommandHandler, CallbackQueryHandler
@@ -13,13 +10,16 @@ logger = logging.getLogger(__name__)
 
 # Adapted from https://github.com/python-telegram-bot/python-telegram-bot/blob/46657afa95bd720bb1319fcb9bc1e8cae82e02b9/examples/inlinekeyboard.py
 
+
+# Global variables generally not a good idea, but make for simple examples
+KEYBOARD = [[InlineKeyboardButton("Option 1", callback_data='1'),
+                InlineKeyboardButton("Option 2", callback_data='2')],
+
+            [InlineKeyboardButton("Option 3", callback_data='3')],
+]
+
 def start(bot, update):
-    keyboard = [[InlineKeyboardButton("Option 1", callback_data='1'),
-                 InlineKeyboardButton("Option 2", callback_data='2')],
-
-                [InlineKeyboardButton("Option 3", callback_data='3')]]
-
-    reply_markup = InlineKeyboardMarkup(keyboard)
+    reply_markup = InlineKeyboardMarkup(KEYBOARD)
 
     update.message.reply_text('Please choose:', reply_markup=reply_markup)
 
@@ -28,12 +28,13 @@ def button(bot, update):
 
     bot.editMessageText(text="Selected option: %s" % query.data,
                         chat_id=query.message.chat_id,
-                        message_id=query.message.message_id)
+                        message_id=query.message.message_id,
+                        reply_markup=InlineKeyboardMarkup(KEYBOARD))
 
 class CallbackTestBeard(Beard):
     """Simple callback button example for skybeard-2
 
-    Type /start to begin."""
+    Type /callbackstart to begin."""
 
     def initialise(self):
         self.disp.add_handler(CommandHandler("callbackstart", start))
