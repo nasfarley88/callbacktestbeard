d6f691463df3b33717a9e5f60b7135cbc8d39fd0
Nathanael Farley <nasfarley88@gmail.com>
2016-11-09 21:14:26 +0000

    Moved functions and vars to be methods of class.

--- __init__.py
+++ __init__.py
@@ -11,32 +11,29 @@ logger = logging.getLogger(__name__)
 # Adapted from https://github.com/python-telegram-bot/python-telegram-bot/blob/46657afa95bd720bb1319fcb9bc1e8cae82e02b9/examples/inlinekeyboard.py
 
 
-# Global variables generally not a good idea, but make for simple examples
-KEYBOARD = [[InlineKeyboardButton("Option 1", callback_data='1'),
-                InlineKeyboardButton("Option 2", callback_data='2')],
-
-            [InlineKeyboardButton("Option 3", callback_data='3')],
-]
-
-def start(bot, update):
-    reply_markup = InlineKeyboardMarkup(KEYBOARD)
-
-    update.message.reply_text('Please choose:', reply_markup=reply_markup)
-
-def button(bot, update):
-    query = update.callback_query
-
-    bot.editMessageText(text="Selected option: %s" % query.data,
-                        chat_id=query.message.chat_id,
-                        message_id=query.message.message_id,
-                        reply_markup=InlineKeyboardMarkup(KEYBOARD))
-
 class CallbackTestBeard(Beard):
     """Simple callback button example for skybeard-2
 
     Type /callbackstart to begin."""
 
     def initialise(self):
-        self.disp.add_handler(CommandHandler("callbackstart", start))
-        self.disp.add_handler(CallbackQueryHandler(button))
+        self.disp.add_handler(CommandHandler("callbackstart", self.start))
+        self.disp.add_handler(CallbackQueryHandler(self.button))
 
+        self.keyboard = InlineKeyboardMarkup([
+            [InlineKeyboardButton("Option 1", callback_data='1'),
+             InlineKeyboardButton("Option 2", callback_data='2')],
+            [InlineKeyboardButton("Option 3", callback_data='3')],
+        ])
+
+
+    def start(self, bot, update):
+        update.message.reply_text('Please choose:', reply_markup=self.keyboard)
+
+    def button(self, bot, update):
+        query = update.callback_query
+
+        bot.editMessageText(text="Selected option: %s" % query.data,
+                            chat_id=query.message.chat_id,
+                            message_id=query.message.message_id,
+                            reply_markup=self.keyboard)
