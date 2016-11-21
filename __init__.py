cdfec5ca78ada378c5aa613b81097054d56ed579
Nathanael Farley <nasfarley88@gmail.com>
2016-11-21 21:48:29 +0000

    Changed for use with telepot library.

--- __init__.py
+++ __init__.py
@@ -1,39 +1,49 @@
 import logging
 
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import MessageHandler, Filters, CommandHandler, CallbackQueryHandler
-from telegram.ext.dispatcher import run_async
-from skybeard.beards import Beard
+import telepot
+import telepot.aio
+from telepot import glance
+from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+from skybeard.beards import BeardAsyncChatHandlerMixin
 
 
 logger = logging.getLogger(__name__)
 
 # Adapted from https://github.com/python-telegram-bot/python-telegram-bot/blob/46657afa95bd720bb1319fcb9bc1e8cae82e02b9/examples/inlinekeyboard.py
+# And then adapted for telepot
 
 
-class CallbackTestBeard(Beard):
-    """Simple callback button example for skybeard-2
+class CallbackTestBeard(telepot.aio.helper.ChatHandler,
+                        BeardAsyncChatHandlerMixin):
+    '''Simple callback button example for skybeard-2
 
-    Type /callbackstart to begin."""
+    Type /callbackstart to begin.
 
-    def initialise(self):
-        self.disp.add_handler(CommandHandler("callbackstart", self.start))
-        self.disp.add_handler(CallbackQueryHandler(self.button))
+    '''
 
-        self.keyboard = InlineKeyboardMarkup([
-            [InlineKeyboardButton("Option 1", callback_data='1'),
-             InlineKeyboardButton("Option 2", callback_data='2')],
-            [InlineKeyboardButton("Option 3", callback_data='3')],
-        ])
+    _timeout = 10
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.register_command('callbackstart', self.start)
 
-    def start(self, bot, update):
-        update.message.reply_text('Please choose:', reply_markup=self.keyboard)
+        self.keyboard = InlineKeyboardMarkup(
+            inline_keyboard=[
+                [InlineKeyboardButton(text='Option 1', callback_data='1'),
+                 InlineKeyboardButton(text='Option 2', callback_data='2')],
+                [InlineKeyboardButton(text='Option 3', callback_data='3')],
+            ])
 
-    def button(self, bot, update):
-        query = update.callback_query
+    __userhelp__ = '''/callbackstart to test callbacks.'''
 
-        bot.editMessageText(text="Selected option: %s" % query.data,
-                            chat_id=query.message.chat_id,
-                            message_id=query.message.message_id,
-                            reply_markup=self.keyboard)
+    async def start(self, msg):
+        await self.sender.sendMessage('Please choose:',
+                                      reply_markup=self.keyboard)
+
+    async def on_callback_query(self, msg):
+        query_id, from_id, query_data = glance(msg, flavor='callback_query')
+
+        await self.bot.editMessageText(
+            telepot.origin_identifier(msg),
+            text="Selected option: {}".format(query_data),
+            reply_markup=self.keyboard)
